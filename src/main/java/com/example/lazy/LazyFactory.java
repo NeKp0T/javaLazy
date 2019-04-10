b50c14eec70249bf8dda8ada52dfbd97413afe9b
Zuev Maxim <max-zu99@mail.ru>
2019-04-10 16:33:21 +0300

    add SingleThreadLazy

--- src/main/java/com/example/lazy/LazyFactory.java
+++ src/main/java/com/example/lazy/LazyFactory.java
@@ -1,10 +1,12 @@
 package com.example.lazy;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.function.Supplier;
 
 public class LazyFactory {
-    public static <T> Lazy<T> createOneThreadLazy(Supplier<T> supplier) {
-
+    public static <T> Lazy<T> createSingleThreadLazy(@NotNull Supplier<T> supplier) {
+        return new SingleThreadLazy<>(supplier);
     }
     public static <T> Lazy<T> createMultiThreadLazy(Supplier<T> supplier) {
 
---
+++ src/main/java/com/example/lazy/SingleThreadLazy.java
@@ -0,0 +1,24 @@
+package com.example.lazy;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
+
+public class SingleThreadLazy<T> implements Lazy<T> {
+    private Supplier<T> supplier;
+    private T result;
+
+    public SingleThreadLazy(@NotNull Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+
+    public T get() {
+        if (supplier != null) {
+            result = supplier.get();
+            supplier = null;
+        }
+        return result;
+    }
+
+
+}
